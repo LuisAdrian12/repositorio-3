d0bc4238e2e803bcbeb5480ef91d0a98b0ce7d48
LuisAdrian12 <124861192+LuisAdrian12@users.noreply.github.com>
2023-05-11 14:56:49 -0600

    Add files via upload

---
+++ 11-05-2023/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Dim dibujar As Boolean = False
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        dibujar = True
+    End Sub
+
+    Private Sub Panel1_MouseMove(Sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If dibujar Then
+            Panel1.CreateGraphics.FillEllipse(Brushes.Red, e.X, e.Y, 5, 5)
+        End If
+    End Sub
+
+    Private Sub Panel1_MouseUp(Sender As Object, e As MouseEventArgs) Handles Panel1.MouseUp
+        dibujar = False
+    End Sub
+
+End Class
